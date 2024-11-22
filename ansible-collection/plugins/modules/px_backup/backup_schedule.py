6ef779dbd08bdcc6cdc53998ecfb45e0ab4fdc13
Parveen <prp@purestorage.com>
2024-11-22 07:46:44 +0000

    resolve comments

--- ansible-collection/plugins/modules/px_backup/backup_schedule.py
+++ ansible-collection/plugins/modules/px_backup/backup_schedule.py
@@ -19,7 +19,6 @@ version_added: "2.8.1"
 description: 
     - Manage backup Schedule in PX-Backup
     - Supports create, update, delete, and list operations
-    - Requires cloud credentials
 
 options:
     api_url:
@@ -33,11 +32,12 @@ options:
     operation:
         description: 
             - Operation to be perform
-            - 'CREATE' will create the backup schedule
-            - 'DELETE' will remove the backup schedule
-            - 'UPDATE' will update the backup schedule
-        choices: ['CREATE', 'DELETE', 'UPDATE']
-        default: CREATE
+            - 'CREATE'  create new backup Schedule
+            - 'DELETE'  delete backup Schedule
+            - 'UPDATE'  update backup Schedule
+            - 'INSPECT_ALL' lists all backup Schedule
+            - 'INSPECT_ONE' retrieves details of a specific backup Schedule
+        choices: ['CREATE', 'DELETE', 'UPDATE','INSPECT_ALL','INSPECT_ONE']
         type: str
     name:
         description: Name of the backup schedule
@@ -48,7 +48,7 @@ options:
         required: true
         type: str
     uid:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Unique identifier of the backup Schedule (required for update/delete)
         required: false
         type: str
     owner:
@@ -56,108 +56,108 @@ options:
         required: true
         type: str
     schedule_policy:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Schedule policy of backup Schedule
         required: false
         type: str
     reclaim_policy:
-        description: Unique identifier of the backup location (required for update/delete)
-        required: false
+        description: Reclaim policy of backup Schedule
+        choices: ['Invalid', 'Delete', 'Retain']
         type: str
     backup_location:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Backup Location of backup Schedule
         required: false
         type: str
     cluster:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Cluster Name
         required: false
         type: str
     namespaces:
-        description: Labels to attach to the backup location
+        description: Namespaces which need to backup
         required: false
         type: dict
     label_selectors:
-        description: Labels to attach to the backup location
+        description: Label Selector for backup
         required: false
         type: dict
     pre_exec_rule:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Pre exec Rule name
         required: false
         type: str
     post_exec_rule:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Post exec Rule name
         required: false
         type: str
     csi_snapshot_class_name:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: CSI Snapshot Class Name
         required: false
         type: str
     include_resources:
-        description: Labels to attach to the backup location
+        description: Resources included for backup
         required: false
         type: dict
     resource_types:
-        description: Labels to attach to the backup location
+        description: Type of Resources for backup
         required: false
         type: dict
     schedule_policy_ref:
-        description: Labels to attach to the backup location
+        description: Schedule Policy Refs
         required: false
         type: dict
     backup_location_ref:
-        description: Labels to attach to the backup location
+        description: Backup Location Ref
         required: false
         type: dict
     pre_exec_rule_ref:
-        description: Labels to attach to the backup location
+        description: Pre Rule Ref
         required: false
         type: dict
     post_exec_rule_ref:
-        description: Labels to attach to the backup location
+        description: Post Rule Ref
         required: false
         type: dict
     cluster_ref:
-        description: Labels to attach to the backup location
+        description: Cluster Ref
         required: false
         type: dict
     backup_type:
-        description: Unique identifier of the backup location (required for update/delete)
-        required: false
+        description: Type of Backup
+        choices: ['Invalid', 'Generic', 'Normal'
         type: str
     ns_label_selectors:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Namespace Label Selectors
         required: false
         type: str
     backup_object_type:
-        description: Labels to attach to the backup location
-        required: false
+        description: Backup Object types
+        choices: ['Invalid', 'All', 'VirtualMachine']
         type: dict
     volume_snapshot_class_mapping:
-        description: Labels to attach to the backup location
+        description: Volume Snapshot Class Mapping
         required: false
         type: dict
     skip_vm_auto_exec_rules:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: Skip VM rules
         required: false
-        type: str
+        type: bool
     direct_kdmp:
-        description: Unique identifier of the backup location (required for update/delete)
+        description: KDMP enable
         required: false
-        type: str
+        type: bool
     validate_certs:
         description: Verify SSL certificates
         type: bool
         default: true
     labels:
-        description: Labels to attach to the backup location
+        description: Labels to attach to the Backup Schedule
         required: false
         type: dict
     ownership:
-        description: Ownership configuration for the backup location
+        description: Ownership configuration for the Backup Schedule
         required: false
         type: dict
         suboptions:
             owner:
-                description: Owner of the backup location
+                description: Owner of the Backup Schedule
                 type: str
             groups:
                 description: List of group access configurations
@@ -191,6 +191,52 @@ options:
                         description: Public access type
                         choices: ['Invalid', 'Read', 'Write', 'Admin']
                         type: str
+    enumerate_options:
+        description: Enumerate Options for the Backup Schedule
+        required: false
+        type: dict
+        suboptions:
+            max_objects:
+                description: Max Object of the Backup Schedule
+                type: str
+            name_filter:
+                description: Backup Schedule name Filter
+                type: str
+            cluster_name_filter:
+                description: Cluster Name filter for the Backup Schedule
+                type: str
+            object_index:
+                description: Object Index of the Backup Schedule
+                type: str
+            include_detailed_resources:
+                description: Resources include for the Backup Schedule
+                type: str
+            cluster_uid_filter:
+                description: Cluster Filter of the Backup Schedule
+                type: str
+            owners:
+                description: Owner of the Backup Schedule
+                type: str
+            backup_object_type
+                description: Backup type of the Backup Schedule
+                choices: ['Invalid', 'Read', 'Write', 'Admin']
+                type: str
+            status
+                description: Status of the Backup Schedule
+                choices: ['Invalid', 'Read', 'Write', 'Admin']
+                type: str
+            time_range:
+                description: Time Range fillter configurations
+                type: list
+                elements: dict
+                suboptions:
+                    start_time:
+                        description: Start time of Backup Schedule
+                        type: str
+                    end_time:
+                        description: End time of Backup Schedule
+                        type: str
+
 '''
 
 def create_backup_schedule(module, client):
@@ -204,7 +250,7 @@ def create_backup_schedule(module, client):
         module.fail_json(msg=f"Failed to create Backup Schedule: {str(e)}")
 
 def update_backup_schedule(module, client):
-    """Update an existing backup location"""
+    """Update an existing backup schedule"""
     backup_schedule_request = backup_schedule_request_body(module)
     backup_schedule_request['metadata']['uid'] = module.params['uid']
     
@@ -215,35 +261,40 @@ def update_backup_schedule(module, client):
         module.fail_json(msg=f"Failed to update Backup Schedule: {str(e)}")
 
 def enumerate_backup_schedules(module, client):
-    """List all backup locations"""
-    params={}
-    if module.params.get('backup_location_ref'):
-        backup_location_ref = module.params['backup_location_ref']
+    """List all backup schedule"""
+    backup_location_ref = module.params.get('backup_location_ref', {})
+    enumerate_options = module.params.get('enumerate_options', {})
+    params ={}
+
+    if backup_location_ref:
         params['backup_location_ref.name'] = backup_location_ref.get('name')
         params['backup_location_ref.uid'] = backup_location_ref.get('uid')
 
-    if module.params.get('enumerate_options'):
-        enumerate_options = module.params['enumerate_options']
-        params['enumerate_options.max_objects']= enumerate_options.get('enumerate_options.max_objects')
-        params['enumerate_options.name_filter']= enumerate_options.get('enumerate_options.name_filter')
-        params['enumerate_options.cluster_name_filter']= enumerate_options.get('enumerate_options.cluster_name_filter')
-        params['enumerate_options.object_index']= enumerate_options.get('enumerate_options.object_index')
-        params['enumerate_options.include_detailed_resources']= enumerate_options.get('enumerate_options.include_detailed_resources')
-        params['enumerate_options.cluster_uid_filter']= enumerate_options.get('enumerate_options.cluster_uid_filter')
-        params['enumerate_options.owners']= enumerate_options.get('enumerate_options.owners')
-        params['enumerate_options.backup_object_type']= enumerate_options.get('enumerate_options.backup_object_type')
-        params['enumerate_options.status']= enumerate_options.get('enumerate_options.status')
-        params['enumerate_options.time_range.start_time']= enumerate_options.get('enumerate_options.time_range.start_time')
-        params['enumerate_options.time_range.end_time']= enumerate_options.get('enumerate_options.time_range.end_time')
+    if enumerate_options:
+        time_range = enumerate_options.get("time_range", {})
+        if time_range:
+            params['enumerate_options.time_range.start_time']= time_range.get('start_time')
+            params['enumerate_options.time_range.end_time']= time_range.get('end_time')
+
+        params['enumerate_options.backup_object_type']= enumerate_options.get('backup_object_type')
+        params['enumerate_options.max_objects']= enumerate_options.get('max_objects')
+        params['enumerate_options.name_filter']= enumerate_options.get('name_filter')
+        params['enumerate_options.cluster_name_filter']= enumerate_options.get('cluster_name_filter')
+        params['enumerate_options.object_index']= enumerate_options.get('object_index')
+        params['enumerate_options.include_detailed_resources']= enumerate_options.get('include_detailed_resources')
+        params['enumerate_options.cluster_uid_filter']= enumerate_options.get('cluster_uid_filter')
+        params['enumerate_options.owners']= enumerate_options.get('owners')
+        params['enumerate_options.status']= enumerate_options.get('status')
+        
     try:
         response = client.make_request('GET', f"v1/backupschedule/{module.params['org_id']}", params=params)
-        # backup_schedules= response.get('backup_schedules')
+
         return response['backup_schedules']
     except Exception as e:
         module.fail_json(msg=f"Failed to enumerate Backup Schedule: {str(e)}")
 
 def inspect_backup_schedules(module, client):
-    """Get details of a specific backup location"""
+    """Get details of a specific backup schedule"""
     params = {
         'uid': module.params.get('uid')
     }
@@ -258,7 +309,7 @@ def inspect_backup_schedules(module, client):
         module.fail_json(msg=f"Failed to inspect Backup Schedule: {str(e)}")
 
 def delete_backup_schedules(module, client):
-    """Delete a backup location"""
+    """Delete a backup schedule"""
     params = {
         'uid': module.params.get('uid')
     }
@@ -272,7 +323,7 @@ def delete_backup_schedules(module, client):
         module.fail_json(msg=f"Failed to delete Backup Schedule: {str(e)}")
 
 def backup_schedule_request_body(module):
-    """Build the Cloud Credential request object"""
+    """Build the backup schedule request object"""
     backup_schedule_request = {
         "metadata": {
             "name": module.params['name'],
@@ -324,19 +375,19 @@ def run_module():
     module_args = dict(
         api_url=dict(type='str', required=True),
         token=dict(type='str', required=True, no_log=True),
-        operation=dict(type='str', choices=['CREATE', 'UPDATE', 'DELETE','INSPECT_ALL','INSPECT_ONE'], default='CREATE'),
+        operation=dict(type='str', choices=['CREATE', 'UPDATE', 'DELETE','INSPECT_ALL','INSPECT_ONE'], required=True),
         name=dict(type='str', required=False),
         org_id=dict(type='str', required=True),
         uid=dict(type='str', required=False),
         owner=dict(type='str', required=False),
         schedule_policy=dict(type='str', required=False),
-        reclaim_policy=dict(type='str', required=False),
+        reclaim_policy=dict(type='str', choices=['Invalid', 'Delete', 'Retain']),
         backup_location=dict(type='str', required=False),
         cluster=dict(type='str', required=False),
         pre_exec_rule=dict(type='str', required=False),
         post_exec_rule=dict(type='str', required=False),
         csi_snapshot_class_name=dict(type='str', required=False),
-        backup_type=dict(type='str', required=False),
+        backup_type=dict(type='str', choices=['Invalid', 'Generic', 'Normal']),
         ns_label_selectors=dict(type='str', required=False),
         skip_vm_auto_exec_rules=dict(type='bool', required=False),
         suspend=dict(type='str', required=False),
@@ -352,7 +403,7 @@ def run_module():
         backup_object_type=dict(
             type='dict',
             options=dict(
-                type=dict(type='str', required=False),
+                type=dict(type='str', choices=['Invalid', 'All', 'VirtualMachine']),
             ),
         ),
         cluster_ref=dict(
@@ -392,7 +443,7 @@ def run_module():
         ),
         resource_types=dict(type='list', elements='str', required=False),
         namespaces=dict(type='list', elements='str'),
-        volume_snapshot_class_mapping=dict(type='dict', required=False, default={}),
+        volume_snapshot_class_mapping=dict(type='dict', required=False),
 
         validate_certs=dict(type='bool', default=True),
         label_selectors=dict(type='dict', required=False),
@@ -409,6 +460,21 @@ def run_module():
             )),
             public=dict(type='dict', options=dict(
                 type=dict(type='str', choices=['Invalid', 'Read', 'Write', 'Admin'])
+            ))
+        )),
+        enumerate_options=dict(type='dict', required=False, options=dict(
+            owners=dict(type='str'),
+            max_objects=dict(type='str'),
+            name_filter=dict(type='str'),
+            cluster_name_filter=dict(type='str'),
+            object_index=dict(type='str'),
+            include_detailed_resources=dict(type='bool'),
+            cluster_uid_filter=dict(type='str'),
+            backup_object_type=dict(type='str',choices=['Invalid', 'All', 'VirtualMachine']),
+            status=dict(type='str'),
+            time_range=dict(type='dict', options=dict(
+                start_time=dict(type='str'),
+                end_time=dict(type='str')
             ))
         )),
     )
@@ -443,30 +509,30 @@ def run_module():
         # Handle other states
         if module.params['operation'] == 'CREATE':
             backup_schedule, changed = create_backup_schedule(module, client)
-            result['message'] = "Backup location created successfully"
+            result['message'] = "Backup schedule created successfully"
 
         elif module.params['operation'] == 'UPDATE':
             # Update existing backup location
             backup_schedule, changed = update_backup_schedule(module, client)
-            result['message'] = "Backup location updated successfully"
+            result['message'] = "Backup schedule updated successfully"
 
         elif module.params['operation'] == 'INSPECT_ALL':
             # Update existing backup location
             backup_schedules = enumerate_backup_schedules(module, client)
-            message=f"Found {len(backup_schedules)} backup locations"
+            message=f"Found {len(backup_schedules)} backup schedules"
             result['message'] = message
             result['backup_schedules']= backup_schedules
 
         elif module.params['operation'] == 'INSPECT_ONE':
             # Update existing backup location
             backup_schedule = inspect_backup_schedules(module, client)
-            result['message'] = "Backup location updated successfully"
+            result['message'] = "Backup schedule Found successfully"
             result['backup_schedule']= backup_schedule
 
         elif module.params['operation'] == 'DELETE':
             # Update existing backup location
             backup_schedule, changed = delete_backup_schedules(module, client)
-            result['message'] = "Backup location updated successfully"
+            result['message'] = "Backup schedule deleted successfully"
 
 
 
